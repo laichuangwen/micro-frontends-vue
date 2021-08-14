3ecce6ca96eb53cb3d3ab2e22596e547a23f50a2
ewen <laichuangwen@cucosmart.com>
2021-08-14 18:02:46 +0800

    文档

--- scripts/commitmsg.js
+++ scripts/commitmsg.js
@@ -3,7 +3,6 @@ const getGitWorkspaceFiles = require('./task/getGitWorkspaceFiles');
 const checkGitUser = require('./task/checkGitUser');
 const checkCommitMsg = require('./task/checkCommitMsg');
 
-console.log('ddddddd');
 manager
     .task('读取工作区文件', getGitWorkspaceFiles('ACDMRTUB'))
     .task('检查邮箱、姓名格式', checkGitUser)
