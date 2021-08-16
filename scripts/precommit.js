e218e1f6cc3ee30ab54f057a1dfcca1b3c010eaa
ewen <laichuangwen@cucosmart.com>
2021-08-16 09:10:58 +0800

    格式化代码

--- scripts/precommit.js
+++ scripts/precommit.js
@@ -2,7 +2,7 @@ const manager = require('./task/manager');
 const getGitWorkspaceFiles = require('./task/getGitWorkspaceFiles');
 const lint = require('./lint');
 // const addToGitWorkspace = require('./task/addToGitWorkspace');
-
+console.log('precommit');
 manager
     .task('读取工作区文件', getGitWorkspaceFiles())
     .task('代码检查', lint)
