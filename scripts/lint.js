001157e7cb21f58fb1942edb4d79b24ed17e7251
ewen <laichuangwen@cucosmart.com>
2021-08-16 10:00:26 +0800

    添加eslint修复

--- scripts/lint.js
+++ scripts/lint.js
@@ -6,9 +6,7 @@ module.exports = async ({ ctx, log }) => {
 
     // 2. Lint files. This doesn't modify target files.
     const results = await eslint.lintFiles(ctx.files);
-    //
-    const errorCount = results.map((list) => list.errorCount).reduce((prev, curr) => prev + curr);
-    console.log('errorCount', errorCount);
+
     // 3. Modify the files with the fixed code.
     await ESLint.outputFixes(results);
 
@@ -16,6 +14,9 @@ module.exports = async ({ ctx, log }) => {
     const formatter = await eslint.loadFormatter('stylish');
     const resultText = formatter.format(results);
     console.log(resultText);
+
+    // 统计错误数量
+    const errorCount = results.map((list) => list.errorCount).reduce((prev, curr) => prev + curr);
     if (errorCount > 0) {
         log(chalk.red(`代码lint有${errorCount}个未处理，请根据eslint提示修复后，再提交！若要跳过检验后面加:--no-verify`));
         // 有错误不给提交
